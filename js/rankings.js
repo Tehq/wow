c63603494a1b2ad993268195cdabd4c11164c49c
Tehq <eric.thibodeau26@gmail.com>
2018-03-30 07:43:36 -0500

    Update rankings.js

--- js/rankings.js
+++ js/rankings.js
@@ -1,15 +1,3 @@
-window.onscroll = function() {myFunction()};
-
-var header = document.getElementById("navbar");
-var sticky = header.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-}
 function makeboldnames() {
 	table = document.getElementById("t01");
 	var lenrows = table.rows.length;
